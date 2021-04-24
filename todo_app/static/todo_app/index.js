518c8410085dab086ae7d970aad6332abb262098
AnthonyRedGrave <69057540+AnthonyRedGrave@users.noreply.github.com>
2021-04-24 20:03:34 +0300

    todo_app

--- todo_app/static/todo_app/index.js
+++ todo_app/static/todo_app/index.js
@@ -1,46 +1,51 @@
 function builtToDo(){
     var wrapper_body = document.getElementById('wrapper')
-    var url = 'http://127.0.0.1:8000/api/todo/list/'
+//    var url = 'http://127.0.0.1:8000/api/todo/list/'
+      var url = 'https://taskitransition7.herokuapp.com/api/todo/list/'
     wrapper_body.innerHTML = ''
     fetch(url)
     .then((resp) => resp.json())
     .then(function(data){
-        console.log(data)
-        var list = data
-        for (var i in list){
-        if (list[i].image !== null){
-            var item = `
-                    <div class="item" id="${list[i].id}" style="background: ${list[i].color}">
-                        <span title="${list[i].name}" data-image="${list[i].image}" onclick="moveUp(this)" class="item-text">${list[i].text}</span>
-                        <div class="icons" style="display: flex; justify-content: space-between; padding: 5px; max-width: 160px; width: 100%;">
-                            <div name="#008B8B"><i class="fa fa-circle" style="color:blue" onclick="update_color(this)" aria-hidden="true"></i></div>
-                            <div name="#FF1493"><i class="fa fa-circle" style="color:pink" onclick="update_color(this)" aria-hidden="true"></i></div>
-                            <div name='#ff9218'><i class="fa fa-circle" style="color:yellow" onclick="update_color(this)" aria-hidden="true"></i></div>
-                            <div><i data-bs-toggle="modal" onclick="change_modal(this)" data-bs-target="#exampleModal" class="fa fa-bars"></i></div>
-                            <div><i class="fa fa-hand-paper-o" title="Изменить заметку" onclick="update(this)" aria-hidden="true"></i></div>
-                            <div><i class="fa fa-window-close" title="Удалить заметку" onclick="delete_el(this)"  aria-hidden="true"></i></div>
+        if (data.length > 0){
+            var list = data
+            for (var i in list){
+                if (list[i].image !== null){
+                    var item = `
+                            <div class="item" id="${list[i].id}" style="background: ${list[i].color}"><span title="${list[i].name}" data-image="${list[i].image}" onclick="moveUp(this)" class="item-text">${list[i].text}</span>
+                                <div class="icons" style="display: flex; justify-content: space-between; padding: 5px; max-width: 160px; width: 100%;">
+                                    <div name="#008B8B"><i class="fa fa-circle" style="color:blue" onclick="update_color(this)" aria-hidden="true"></i></div>
+                                    <div name="#FF1493"><i class="fa fa-circle" style="color:pink" onclick="update_color(this)" aria-hidden="true"></i></div>
+                                    <div name='#ff9218'><i class="fa fa-circle" style="color:yellow" onclick="update_color(this)" aria-hidden="true"></i></div>
+                                    <div><i data-bs-toggle="modal" onclick="change_modal(this)" data-bs-target="#exampleModal" class="fa fa-bars"></i></div>
+                                    <div><i class="fa fa-hand-paper-o" title="Изменить заметку" onclick="update(this)" aria-hidden="true"></i></div>
+                                    <div><i class="fa fa-window-close" title="Удалить заметку" onclick="delete_el(this)"  aria-hidden="true"></i></div>
+                                </div>
+                            </div>
+                        `
+                }
+                else{
+                    var item = `
+                        <div class="item" id="${list[i].id}" style="background: ${list[i].color}">
+                            <span title="${list[i].name}" onclick="moveUp(this)" class="item-text">${list[i].text}</span>
+                            <div class="icons" style="display: flex; justify-content: space-between; padding: 5px; max-width: 160px; width: 100%;">
+                                <div name="#008B8B"><i class="fa fa-circle" style="color:blue" onclick="update_color(this)" aria-hidden="true"></i></div>
+                                <div name="#FF1493"><i class="fa fa-circle" style="color:pink" onclick="update_color(this)" aria-hidden="true"></i></div>
+                                <div name='#ff9218'><i class="fa fa-circle" style="color:yellow" onclick="update_color(this)" aria-hidden="true"></i></div>
+                                <div><i data-bs-toggle="modal" onclick="change_modal(this)" data-bs-target="#exampleModal" class="fa fa-bars"></i></div>
+                                <div><i class="fa fa-hand-paper-o" title="Изменить заметку" onclick="update(this)" aria-hidden="true"></i></div>
+                                <div><i class="fa fa-window-close" title="Удалить заметку" onclick="delete_el(this)"  aria-hidden="true"></i></div>
+                            </div>
                         </div>
-                    </div>
-                `
+                    `
+                }
+
+                    wrapper_body.innerHTML += item
+            }
         }
         else{
-            var item = `
-                <div class="item" id="${list[i].id}" style="background: ${list[i].color}">
-                    <span title="${list[i].name}" onclick="moveUp(this)" class="item-text">${list[i].text}</span>
-                    <div class="icons" style="display: flex; justify-content: space-between; padding: 5px; max-width: 160px; width: 100%;">
-                        <div name="#008B8B"><i class="fa fa-circle" style="color:blue" onclick="update_color(this)" aria-hidden="true"></i></div>
-                        <div name="#FF1493"><i class="fa fa-circle" style="color:pink" onclick="update_color(this)" aria-hidden="true"></i></div>
-                        <div name='#ff9218'><i class="fa fa-circle" style="color:yellow" onclick="update_color(this)" aria-hidden="true"></i></div>
-                        <div><i data-bs-toggle="modal" onclick="change_modal(this)" data-bs-target="#exampleModal" class="fa fa-bars"></i></div>
-                        <div><i class="fa fa-hand-paper-o" title="Изменить заметку" onclick="update(this)" aria-hidden="true"></i></div>
-                        <div><i class="fa fa-window-close" title="Удалить заметку" onclick="delete_el(this)"  aria-hidden="true"></i></div>
-                    </div>
-                </div>
-            `
+            wrapper_body.innerHTML = `<div>Заметок нет</div>`
         }
 
-            wrapper_body.innerHTML += item
-        }
     })
 }
 
@@ -49,15 +54,19 @@ builtToDo()
 const new_todo = $(".new_todo")[0]
 const new_todo_input = $('.new_todo_input')[0]
 const new_todo_title = $('.new_todo_title')[0]
+const save_all_button = $('.save_all_button')[0]
+const delete_all_button = $('.delete_all_button')[0]
 const csrf = document.getElementsByName('csrfmiddlewaretoken')
 
 var activeItem = null
 
 new_todo.addEventListener('click', function(e){
     e.preventDefault()
-    var url = 'http://127.0.0.1:8000/api/todo/list/'
+//    var url = 'http://127.0.0.1:8000/api/todo/list/'
+    var url = `https://taskitransition7.herokuapp.com/api/todo/list/`
     if (activeItem != null){
-        var url = `http://127.0.0.1:8000/api/todo/list/${activeItem}/`
+//        var url = `http://127.0.0.1:8000/api/todo/list/${activeItem}/`
+          var url = `https://taskitransition7.herokuapp.com/api/todo/list/${activeItem}/`
         var data = {"name": new_todo_title.value, "text": new_todo_input.value}
         activeItem = null
         $.ajax({
@@ -91,6 +100,8 @@ new_todo.addEventListener('click', function(e){
                 'X-CSRFToken': csrf[0].value,
             },
             success: function (response) {
+                new_todo_title.value = ''
+                new_todo_input.value = ''
                 builtToDo()
             },
             error: function (response) {
@@ -98,14 +109,14 @@ new_todo.addEventListener('click', function(e){
             }
         })
     }
-
 })
 
 function update_color(el){
     const color = el.parentNode.getAttribute("name")
     var id = el.parentNode.parentNode.parentNode.id
     activeItem = id
-    var url = `http://127.0.0.1:8000/api/todo/list/${activeItem}/`
+//    var url = `http://127.0.0.1:8000/api/todo/list/${activeItem}/`
+    var url = `https://taskitransition7.herokuapp.com/api/todo/list/${activeItem}/`
     var data = {"color": color}
         $.ajax({
             type: 'PATCH',
@@ -123,7 +134,6 @@ function update_color(el){
                 console.log(response)
             }
         })
-
 }
 
 function update(el){
@@ -131,13 +141,12 @@ function update(el){
       activeItem = id
       new_todo_title.value = el.parentNode.parentNode.previousElementSibling.title
       new_todo_input.value = el.parentNode.parentNode.previousElementSibling.innerHTML
-
-
 }
 
 function delete_el(el){
     var id = el.parentNode.parentNode.parentNode.id
-    var url = `http://127.0.0.1:8000/api/todo/list/${id}/`
+//    var url = `http://127.0.0.1:8000/api/todo/list/${id}/`
+    var url = `https://taskitransition7.herokuapp.com/api/todo/list/${id}/`
     $.ajax({
         type: 'DELETE',
         url: url,
@@ -169,7 +178,7 @@ function moveUp(el){
 function run () {
   const items = document.querySelectorAll('.item-text')
   const focused = [...items].find(i => i.classList.contains('focused'))
-focused.parentNode.parentNode.prepend(focused.parentNode)
+  focused.parentNode.parentNode.prepend(focused.parentNode)
 
 }
 
@@ -180,24 +189,27 @@ function change_modal(el){
     var text_todo = el.parentNode.parentNode.previousElementSibling.innerHTML
     var data_image = el.parentNode.parentNode.previousElementSibling.getAttribute('data-image')
     var id_todo = el.parentNode.parentNode.parentNode.id
+//    var url = `http://127.0.0.1:8000/upload_image/${id_todo}/`
+    var url = `https://taskitransition7.herokuapp.com/upload_image/${id_todo}/`
+
+    var form = $(".image_form")
+    form[0].action = `upload_image/${id_todo}/`
     if (data_image !== null){
-        console.log('есть картинка')
         modal_body.innerHTML = `<div class="h5 mb-3" data-id="${id_todo}">Заметка</div>
             <div class="text-muted">
                 <ul>
-                    <li>Сложность: <b>${text_todo}</b></li>
-                    <li>Картинка к заметке:
+                    <li>Текст заметки: <b>${text_todo}</b></li>
+                    <li>
                      <image src="${data_image}" width="250" height="150" alt="К заметке не прикреплена картинка"></image></li>
                 </ul>
             </div>
             `
     }
     else{
-        console.log('нет картинки')
         modal_body.innerHTML = `
         <div class="h5 mb-3" data-id="${id_todo}">Заметка</div><div class="text-muted">
                 <ul>
-                    <li>Сложность: <b>${text_todo}</b></li>
+                    <li>Текст заметки: <b>${text_todo}</b></li>
                     <li>Нет картинки</li>
                 </ul>
             </div>
@@ -205,33 +217,59 @@ function change_modal(el){
     }
         $("#exampleModalLabel").html(title_todo)
 }
-$("#id_ajax_upload_form").submit(function(e){
-    e.preventDefault();
 
-    var id = modal_body.firstElementChild.getAttribute("data-id")
-    var file = $("#image_to_upload")[0].files[0]
-    // this - форма для отправления из index.html
-    var formdata = new FormData(this)
-    formdata.append("csrfmiddlewaretoken", "{{ csrf_token }}")
-    formdata.append("image", file)
-    console.log(formdata)
-    var data = {"image": file}
-    var url_to_upload = `http://127.0.0.1:8000/api/todo/list/${id}/`
-    $.ajax({
-                type: 'PATCH',
-                    url: url_to_upload,
-                    data: JSON.stringify(formdata),
-                    processData: false,
-				    contentType: false,
-                    success: function (response) {
-                        console.log(response)
-                        builtToDo()
-                    },
-                    cache: false,
-                    contentType: false,
-                    processData: false
+save_all_button.addEventListener('click', function(e){
+    e.preventDefault()
+    var items = [...document.querySelector('.wrapper').children]
+    for (var i in items){
+        var id = items[i].id
+//        var url = `http://127.0.0.1:8000/api/todo/list/${id}/`
+        var url = `https://taskitransition7.herokuapp.com/api/todo/list/${id}/`
 
+        var data = {"index": i}
+        $.ajax({
+            type: 'PATCH',
+            url: url,
+            data: JSON.stringify(data),
+            dataType: "json",
+            headers: {
+                'Content-type': 'application/json',
+                'X-CSRFToken': csrf[0].value,
+            },
+            success: function (response) {
+                console.log(response)
+            },
+            error: function (response) {
+                console.log(response)
+            }
+        })
 
+    }
+
+})
+
+delete_all_button.addEventListener('click', function(e){
+    var items = [...document.querySelector('.wrapper').children]
+    for (var i in items){
+        var id = items[i].id
+//            var url = `http://127.0.0.1:8000/api/todo/list/${id}/`
+            var url = `https://taskitransition7.herokuapp.com/api/todo/list/${id}/`
+            $.ajax({
+                type: 'DELETE',
+                url: url,
+                dataType: "json",
+                headers: {
+                    'Content-type': 'application/json',
+                    'X-CSRFToken': csrf[0].value,
+                },
+                success: function (response) {
+
+                },
+                error: function (response) {
+                    console.log(response)
+                }
             })
+        }
+        window.location.href = 'https://taskitransition7.herokuapp.com'
 
 })
