4922fee3ffbb294e59eef439a0ef88820f747e92
AnthonyRedGrave <69057540+AnthonyRedGrave@users.noreply.github.com>
2021-04-24 19:53:04 +0300

    final

--- todo/settings.py
+++ todo/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%vt0lh9^u42u9)oq7f5=c#ys0g*sm)#8nh$fcb4x#u*8k7-%8-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['taskitransition7.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -123,6 +123,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_DIRS = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
